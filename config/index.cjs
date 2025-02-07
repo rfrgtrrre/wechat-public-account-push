4f17c3fc19fb9489e65e98b1bc2db6b4e2432b00
rfrgtrrre <111343849+rfrgtrrre@users.noreply.github.com>
2025-02-07 16:30:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ofunt7FcVPEm6k3ntsf7rm_0Frl8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'tc3Lg7jbN7wF9uVFR3glDVjOBpKZSHbNqUWkNYmlB7w',
+      useTemplateId: 'xlBpmIaCLpYYKB_I506oG3f53dQhnBKwEHN2JYQbvbc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-13',
       festivals: [
