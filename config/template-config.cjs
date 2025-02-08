6ae2311180d5a08b9f3a49f9c09c46ffdcc35cc6
rfrgtrrre <111343849+rfrgtrrre@users.noreply.github.com>
2025-02-08 17:59:19 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -24,13 +24,13 @@ const TEMPLATE_CONFIG = [
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      今天是我们相恋的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
       ---
       
-      {{moment_copyrighting.DATA}}
+      {{earthy_love_words.DATA}}
       
       {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
